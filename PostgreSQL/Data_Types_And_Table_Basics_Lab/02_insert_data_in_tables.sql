81eb6e3b3138c9b9e5ec66937ace7476f1a6660e
Vladimir Demirev <v.demirev@yahoo.com>
2024-06-02 18:14:20 +0300

    Solved problems from the lecture Data Types And Tables Basics - Lab

---
+++ PostgreSQL/Data_Types_And_Table_Basics_Lab/02_insert_data_in_tables.sql
@@ -0,0 +1,11 @@
+INSERT INTO employees (first_name, last_name, hiring_date, salary, devices_number)
+VALUES ('Vladimir', 'Demirev', '2024-04-01', '5400', '2' );
+ 
+INSERT INTO employees (first_name, last_name, hiring_date, salary, devices_number)
+VALUES ('Pesho', 'Peshev', '2024-04-01', '5400', '2' );
+ 
+INSERT INTO employees (first_name, last_name, hiring_date, salary, devices_number)
+VALUES ('Gosho', 'Goshov', '2024-04-01', '5400', '2' );
+ 
+INSERT INTO issues (description, date, start)
+VALUES ('dog', '2024-04-01', '2024-06-01 10:20:30');
