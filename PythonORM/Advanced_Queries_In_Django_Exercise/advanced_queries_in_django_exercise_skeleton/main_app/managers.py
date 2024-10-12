56486e00aeb0a8e8a232dc1c8623286a806b4994
Vladimir Demirev <v.demirev@yahoo.com>
2024-10-12 18:21:44 +0300

    Added solution to 02. Video Games Library

--- PythonORM/Advanced_Queries_In_Django_Exercise/advanced_queries_in_django_exercise_skeleton/main_app/managers.py
+++ PythonORM/Advanced_Queries_In_Django_Exercise/advanced_queries_in_django_exercise_skeleton/main_app/managers.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 
 from django.db import models
-from django.db.models import QuerySet, Count
+from django.db.models import QuerySet, Count, Avg
 
 
 class RealEstateListingManager(models.Manager):
@@ -15,9 +15,27 @@ class RealEstateListingManager(models.Manager):
     def with_bedrooms(self, bedrooms_count: int) -> QuerySet:
         return self.filter(bedrooms=bedrooms_count)
 
-    def popular_locations(self):
+    def popular_locations(self) -> QuerySet:
         return self.values("location").annotate(
             location_count=Count("location")
         ).order_by(
             "-location_count", "location"
         )[:2]
+
+
+class VideoGameManager(models.Manager):
+    def games_by_genre(self, genre: str) -> QuerySet:
+        return self.filter(genre=genre)
+
+    def recently_released_games(self, year: int) -> QuerySet:
+        return self.filter(release_year__gte=year)
+
+    def highest_rated_game(self) -> str:
+        return self.order_by("-rating").first()
+
+    def lowest_rated_game(self) -> str:
+        return self.order_by("-rating").last()
+
+    def average_rating(self) -> str:
+        avg_rating = self.aggregate(avg_rating=Avg("rating"))["avg_rating"]
+        return f"{avg_rating:.1f}"
